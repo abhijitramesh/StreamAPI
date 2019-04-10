d7fd4b01aec0acad26feb06a52a624013042ce15
abhijitramesh <abhijitramesh2k@gmail.com>
2019-04-10 22:34:28 +0530

    this is used for mapping all objects in stream with different objects

---
+++ Map.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class Map{
+
+    public static void main(String args[]){
+
+        List<String> newlist = new ArrayList<String>();
+
+
+        newlist.add("One");
+        newlist.add("Two");
+        newlist.add("Three");
+        newlist.add("Four");
+        newlist.add("Five");
+
+
+        Stream<String> stream = newlist.stream();
+
+        Stream<String> stream1= stream.map((values)-> {return values.toUpperCase();
+        });
+
+        stream1.forEach(e -> System.out.println(e));
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
