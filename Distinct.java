b73aa3968988d9c03cb0522deee0b76eaf07591f
abhijitramesh <abhijitramesh2k@gmail.com>
2019-04-11 17:49:47 +0530

    Discting function is used for selecting only elements which are distinct in a Stream and would remove the rest

---
+++ Distinct.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class Distinct{
+
+    public static void main(String args[]){
+
+        List<String> newList = new ArrayList<String>();
+        
+        newList.add("One");
+        newList.add("Two");
+        newList.add("Three");
+        newList.add("One");
+
+
+        Stream<String> stream =  newList.stream();
+
+        stream.distinct().forEach(e -> System.out.println(e));
+ 
+    }
+
+
+}
